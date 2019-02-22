54c078aae2989c476e455943418bb3ae51264cea
jacksonwb <jackson@jacksonwb.com>
2019-02-22 12:31:42 -0700

    update images, add dark mode

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-console.log("JS App has been loaded");
+//So horribly vanilla!
+console.log("Loaded the JS y'all!");
 
-//Set element pointers
+//--- Onload Fade
+window.onload = function (e) {
+	const container = document.querySelector('.container');
+
+	setTimeout(function () {
+		container.style.visibility = "visible";
+		container.style.opacity = 1;
+}, 100);
+};
+
+//--- Email Copy
 const tooltip = document.createElement('div');
 tooltip.setAttribute("class", "tooltip");
 tooltip.style.opacity = 0;
 tooltip.innerHTML = "<p>Email address copied to clipboard</p>";
-
 var copyEmailBtn = document.querySelector('.js-emailcopybtn');  
+const links = document.querySelector('#links');
 
+//--- Email Copy Handler
 copyEmailBtn.addEventListener('click', function(event) {  
-  // Select the email link anchor text  
-  var emailLink = document.querySelector('.js-emailcopybtn');  
-  var range = document.createRange();  
-  range.selectNode(emailLink);  
-  window.getSelection().addRange(range);
-
+  var textArea = document.createElement("textarea");
+  textArea.value = "jbeall@student.42.us.org";
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
   try {  
-    // Now that we've selected the anchor text, execute the copy command  
     var successful = document.execCommand('copy');  
 
-    //Append tooltip element to main div and fade tooltip in and out
-    document.querySelector('.float').appendChild(tooltip);
+	document.querySelector('.float').insertBefore(tooltip, links);
+	tooltip.style.color = darkFlag ? purple : 'yellow';
+	console.log(darkFlag);
     setTimeout(function() {
       tooltip.style.opacity = 1;
     }, 5);
@@ -31,14 +42,56 @@ copyEmailBtn.addEventListener('click', function(event) {
       }, 500);
 	  }, 3000);
 
-    //report success or catch error
     var msg = successful ? 'successful' : 'unsuccessful';  
     console.log('Copy email command was ' + msg);  
   } catch(err) {  
     console.log('Oops, unable to copy');  
   }
+  document.body.removeChild(textArea);
+});
 
-  // Remove the selections - NOTE: Should use
-  // removeRange(range) when it is supported  
-  window.getSelection().removeAllRanges();  
+//--- Set Text Color Helper
+function textColor(color)
+{
+	const top = document.querySelector('.title');
+	const bottom = document.querySelector('.list').children;
+	top.style.color = color;
+	for (let i = 0; i < bottom.length; i++) {
+		bottom[i].style.color = color;
+	}
+}
+
+//--- Dark Mode Toggle
+const purple = '#7E44FF';
+let darkFlag = 0;
+const container = document.querySelector('.container')
+
+//--- Preload sky
+const sky = new Image();
+sky.src = 'images/sky.jpg';
+
+document.getElementById('toggle').addEventListener('click', function(e) {
+	if (!darkFlag) {
+		darkFlag = 1;
+		document.body.style.background = 'black';
+		container.style.opacity = 0;
+		setTimeout(function () {
+			document.getElementById('main-img').style.backgroundImage = "url('images/sky.jpg')";
+			document.querySelector('#toggle').innerHTML = 'light!';
+			document.querySelector('#toggle').style.color = 'yellow';
+			textColor(purple);
+			container.style.opacity = 1;
+		}, 300);
+} else {
+		darkFlag = 0;
+		document.body.style.background = 'white';
+		container.style.opacity = 0;
+		setTimeout(function () {
+			document.getElementById('main-img').style.backgroundImage = "url('images/tree.jpg')";
+			document.querySelector('#toggle').innerHTML = 'dark!';
+			document.querySelector('#toggle').style.color = purple;
+			textColor('yellow');
+			container.style.opacity = 1;
+		}, 300);
+	}
 });
